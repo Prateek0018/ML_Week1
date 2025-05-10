9cb7510242a752d3a5bc431a5c9f6b2ddfd9bbdf
Prateek0018 <prateekjoshi7974@gmail.com>
2025-05-10 23:35:35 +0530

    check whether a number is an armstrong

---
+++ Task_2/Q12.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a number: "))
+original = num
+power = len(str(num))  # Count digits
+result = 0
+
+while num > 0:
+    digit = num % 10
+    result += digit ** power
+    num //= 10
+
+if result == original:
+    print("Armstrong number")
+else:
+    print("Not an Armstrong number")
